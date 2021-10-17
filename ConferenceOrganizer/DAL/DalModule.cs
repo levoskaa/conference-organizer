098945e2afb8d37698fb281b84817b0b6e0b0e9c
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-17 20:22:50 +0200

    feat(dal): add common classes

---
+++ ConferenceOrganizer/DAL/DalModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System.Linq;
+using System.Reflection;
+
+namespace DAL
+{
+    public class DalModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            var assembly = Assembly.GetAssembly(typeof(DalModule));
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(type => type.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+            builder.RegisterType<UnitOfWork>()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ ConferenceOrganizer/DAL/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await dbContext.Database.BeginTransactionAsync();
+        }
+    }
+}
