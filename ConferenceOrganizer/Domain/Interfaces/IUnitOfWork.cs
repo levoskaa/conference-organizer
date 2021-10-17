bc001c0da84e3413e57972ac897f511541483008
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-17 20:15:28 +0200

    feat(domain): add common classes

---
+++ ConferenceOrganizer/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync();
+
+        Task<IDbContextTransaction> BeginTransactionAsync();
+    }
+}
