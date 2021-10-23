f7752eb870d71501287372cea78448a574a6e558
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-23 13:19:34 +0200

    feat(dal): rename default user table

---
+++ ConferenceOrganizer/DAL/EntityConfigurations/ApplicationUserEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.EntityConfigurations
+{
+    public class ApplicationUserEntityTypeConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> userConfiguration)
+        {
+            userConfiguration.ToTable("Users");
+        }
+    }
+}
