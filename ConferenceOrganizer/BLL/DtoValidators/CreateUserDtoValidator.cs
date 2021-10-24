81d54bc1995e886e01f7c03e0dd8c43ecb15aaf1
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-24 11:01:37 +0200

    feat(bll): add initial user create dto

---
+++ ConferenceOrganizer/BLL/DtoValidators/CreateUserDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Dtos;
+using FluentValidation;
+
+namespace BLL.DtoValidators
+{
+    public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+    {
+        public CreateUserDtoValidator()
+        {
+            RuleFor(ent => ent.Username)
+                .NotEmpty().WithMessage("Username is required.");
+            RuleFor(ent => ent.Password)
+                .NotEmpty().WithMessage("Password is required.");
+        }
+    }
+}
