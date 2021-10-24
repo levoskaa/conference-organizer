c7e26d477ed4a45c8081089e67adf297875b35d4
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-24 14:21:29 +0200

    feat(api): add users controller

---
+++ ConferenceOrganizer/ConferenceOrganizer/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Dtos;
+using BLL.Interfaces;
+using BLL.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using Web.Common;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ApiController
+    {
+        private readonly IAuthService authService;
+
+        public UsersController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost]
+        [Authorize("Admin")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public Task<EntityCreatedViewModel> CreateUser([FromBody] CreateUserDto createUserDto)
+        {
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.Created;
+            return authService.CreateUserAsync(createUserDto);
+        }
+    }
+}
