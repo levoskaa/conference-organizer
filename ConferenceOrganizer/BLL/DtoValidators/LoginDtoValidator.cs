1b160de51dd54121fcaedffdd2996babdeea600e
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-24 11:01:15 +0200

    feat(bll): add login dto

---
+++ ConferenceOrganizer/BLL/DtoValidators/LoginDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Dtos;
+using FluentValidation;
+
+namespace BLL.DtoValidators
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(ent => ent.Username)
+                .NotEmpty().WithMessage("Username is required.");
+            RuleFor(ent => ent.Password)
+                .NotEmpty().WithMessage("Password is required.");
+        }
+    }
+}
