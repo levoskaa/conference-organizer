9aeaeed76d569e8bbd2f83994bdfd7552819f319
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-24 21:38:28 +0200

    feat(dal): add conference repository

---
+++ ConferenceOrganizer/DAL/Repositories/ConferenceRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entitites;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class ConferenceRepository : IConferenceRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ConferenceRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Conference>> GetConferencesAsync()
+        {
+            var conferences = await dbContext.Conferences.ToListAsync();
+            return conferences;
+        }
+    }
+}
---
+++ ConferenceOrganizer/Domain/Interfaces/IConferenceRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entitites;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IConferenceRepository
+    {
+        Task<IEnumerable<Conference>> GetConferencesAsync();
+    }
+}
