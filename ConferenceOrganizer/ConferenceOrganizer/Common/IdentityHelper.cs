87c99b8976c2faac61644337d3a0122d6d5b84d8
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-24 14:05:00 +0200

    feat(api): add helper for getting user identity

---
+++ ConferenceOrganizer/ConferenceOrganizer/Common/IdentityHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Web.Common
+{
+    public class IdentityHelper
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public IdentityHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public int GetAuthenticatedUserId()
+        {
+            return int.Parse(httpContextAccessor.HttpContext.User.FindFirstValue(JwtRegisteredClaimNames.Sub));
+        }
+    }
+}
