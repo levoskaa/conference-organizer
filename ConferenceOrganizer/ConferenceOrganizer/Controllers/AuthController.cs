a8836d7792fbee595f04ba3cb10b19fb0e2b6176
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-10-24 14:21:16 +0200

    feat(api): add auth controller

---
+++ ConferenceOrganizer/ConferenceOrganizer/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using BLL.Dtos;
+using BLL.Interfaces;
+using BLL.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Web.Common;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : ApiController
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public Task<TokenViewModel> Login([FromBody] LoginDto loginDto)
+        {
+            return authService.LoginAsync(loginDto);
+        }
+    }
+}
