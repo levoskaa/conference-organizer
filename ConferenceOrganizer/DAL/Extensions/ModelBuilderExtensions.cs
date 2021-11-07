54640a95af16eca82d7b26174dae7c19ab077ee2
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-11-07 11:07:35 +0100

    feat(dal): add seed users

--- ConferenceOrganizer/DAL/Extensions/ModelBuilderExtensions.cs
+++ ConferenceOrganizer/DAL/Extensions/ModelBuilderExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 namespace DAL.Extensions
 {
@@ -9,6 +10,40 @@ namespace DAL.Extensions
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
+            #region Users
+
+            var users = new List<ApplicationUser>();
+            var passwordHasher = new PasswordHasher<ApplicationUser>();
+
+            var admin = new ApplicationUser
+            {
+                Id = 1,
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            admin.PasswordHash = passwordHasher.HashPassword(admin, "abc123");
+            users.Add(admin);
+
+            for (int i = 2; i <= 20; i++)
+            {
+                var user = new ApplicationUser
+                {
+                    Id = i,
+                    UserName = $"user{i}",
+                    NormalizedUserName = $"USER{i}",
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                user.PasswordHash = passwordHasher.HashPassword(user, "abc123");
+                users.Add(user);
+            }
+            modelBuilder.Entity<ApplicationUser>()
+                    .HasData(users);
+
+            #endregion Users
+
+            #region Roles
+
             modelBuilder.Entity<ApplicationRole>()
                 .HasData(new ApplicationRole
                 {
@@ -17,23 +52,14 @@ namespace DAL.Extensions
                     NormalizedName = "ADMIN"
                 });
 
-            var passwordHasher = new PasswordHasher<ApplicationUser>();
-            var user = new ApplicationUser
-            {
-                Id = 1,
-                UserName = "admin",
-                NormalizedUserName = "ADMIN",
-                SecurityStamp = Guid.NewGuid().ToString()
-            };
-            user.PasswordHash = passwordHasher.HashPassword(user, "abc123");
-            modelBuilder.Entity<ApplicationUser>()
-                .HasData(user);
             modelBuilder.Entity<IdentityUserRole<int>>()
                 .HasData(new IdentityUserRole<int>
                 {
                     RoleId = 1,
                     UserId = 1
                 });
+
+            #endregion Roles
         }
     }
 }
