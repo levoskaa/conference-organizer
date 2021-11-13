9c7f796fb399237e53078bfbb437866302d16432
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-11-13 22:26:30 +0100

    feat(bll): add RoomUpsertDto validator

---
+++ ConferenceOrganizer/BLL/DtoValidators/RoomUpsertDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using BLL.Dtos;
+using FluentValidation;
+
+namespace BLL.DtoValidators
+{
+    public class RoomUpsertDtoValidator : AbstractValidator<RoomUpsertDto>
+    {
+        public RoomUpsertDtoValidator()
+        {
+            RuleFor(ent => ent.UniqueName)
+                .NotEmpty().WithMessage("Name is required.");
+            RuleFor(ent => ent.Capacity)
+                .NotEmpty().WithMessage("Capacity is required.");
+        }
+    }
+}
