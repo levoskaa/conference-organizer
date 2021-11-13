44573fe039ff7b79446c45b045ae066d0a5746b5
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-11-13 17:41:20 +0100

    fix(field): add uspert dto validator

---
+++ ConferenceOrganizer/BLL/DtoValidators/ProfessionalFieldUpsertDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using BLL.Dtos;
+using FluentValidation;
+
+namespace BLL.DtoValidators
+{
+    public class ProfessionalFieldUpsertDtoValidator : AbstractValidator<ProfessionalFieldUpsertDto>
+    {
+        public ProfessionalFieldUpsertDtoValidator()
+        {
+            RuleFor(ent => ent.Name)
+                .NotEmpty().WithMessage("Name is required.");
+        }
+    }
+}
