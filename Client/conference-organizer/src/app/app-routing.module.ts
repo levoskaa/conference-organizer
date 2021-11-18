12b1afefd7b097c032eb4a0d112ddd490712e470
Schütz Máté <schutz.mate@gmail.com>
2021-11-18 09:50:13 +0100

    feat(client): add routing guards

--- Client/conference-organizer/src/app/app-routing.module.ts
+++ Client/conference-organizer/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { UserAuthGuard } from './guards/user-auth.guard';
 
 const routes: Routes = [
     { path: 'login', component: LoginPageComponent },
-    { path: 'profile', component: ProfilePageComponent },
+    { path: 'profile', component: ProfilePageComponent, canActivate: [UserAuthGuard] },
     { path: '', pathMatch: 'full', component: HomeComponent }
 ];
 
---
+++ Client/conference-organizer/src/app/guards/user-auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserAuthGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        if (localStorage.getItem("access_token") !== null) {
+            return true;
+        }
+        this.router.navigate(['signin']);
+        return false;
+    }
+
+}
