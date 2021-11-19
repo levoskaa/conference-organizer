339c5a80f4978f4fec048e36987efce5f8679ec0
levoskaa <47329402+levoskaa@users.noreply.github.com>
2021-11-19 17:13:09 +0100

    feat(conferences): generate conference details component

---
+++ Client/conference-organizer/src/app/components/conference-details/conference-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conference-details',
+  templateUrl: './conference-details.component.html',
+  styleUrls: ['./conference-details.component.scss']
+})
+export class ConferenceDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
